f56c8091641ff422a363d77488558a588d4f8803
ZidAvwa <zidaab26@gmail.com>
2026-03-03 21:51:08 +0700

    End of JS 3

--- WebProgramming/4thSemester/POS/routes/web.php
+++ WebProgramming/4thSemester/POS/routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelContoroller;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/level', [LevelContoroller::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
