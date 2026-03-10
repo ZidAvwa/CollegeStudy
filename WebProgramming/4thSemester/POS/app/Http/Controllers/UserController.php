7da8c195c1ddb5958f6ea16bf7f0d37c2b1ecfca
ZidAvwa <zidaab26@gmail.com>
2026-03-10 21:03:07 +0700

    Practicum-2.5

--- WebProgramming/4thSemester/POS/app/Http/Controllers/UserController.php
+++ WebProgramming/4thSemester/POS/app/Http/Controllers/UserController.php
@@ -10,12 +10,39 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'nama' => 'Pelanggan Pertama'
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        $user = UserModel::create(
+            [
+                'username' => 'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->username = 'manager12';
 
-        $user = UserModel::all();
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
+        // $user->isDirty(); //t
+        // $user->isDirty('username'); //t
+        // $user->isDirty('nama'); //f
+        // $user->isDirty(['nama', 'username']); //t
+
+        // $user->isDirty(); //f
+        // $user->isDirty('username'); //f
+        // $user->isDirty('nama'); //t
+        // $user->isDirty(['nama', 'username']); //f
+
+        // $user->save();
+
+        // $user->isDirty(); //f
+        // $user->isClean(); //t
+        // dd($user->isDirty());
+
         return view('user', ['data' => $user]);
     }
 }
